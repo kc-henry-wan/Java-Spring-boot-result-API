efd747efe94ca2adaaed148804f6fed238248644
kc-henry-wan <henrykcwan@gmail.com>
2024-11-05 13:32:22 +0000

    Update JobRepository.java

--- src/main/java/com/hope/apiapp/repository/JobRepository.java
+++ src/main/java/com/hope/apiapp/repository/JobRepository.java
@@ -10,6 +10,17 @@ import com.hope.apiapp.model.Job;
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
+	@Query("SELECT j.jobId AS jobId, j.jobRef AS jobRef, j.jobDate AS jobDate, j.hourlyRate AS hourlyRate, "
+						+ "j.totalWorkHour AS totalWorkHour, j.totalPaid AS totalPaid, "
+						+ "j.lunchArrangement AS lunchArrangement, j.parkingOption AS parkingOption, "
+						+ "j.ratePerMile AS ratePerMile, j.status AS status, "
+						+ "pb.branchName AS branchName, pb.address1 AS branchAddress1, "
+						+ "pb.address2 AS branchAddress2, pb.postalCode AS branchPostalCode, "
+						+ "pb.latitude AS branchLatitude, pb.longitude AS branchLongitude "
+						+ "FROM Job j JOIN j.pharmacyBranch pb "
+	      + "WHERE j.jobId = :jobId ")
+	List<JobDTO> getJobByIdWithLimitedFields(@Param("jobId") Long jobId);
+	
 	Optional<Job> findById(Long JobId);
 
 }
